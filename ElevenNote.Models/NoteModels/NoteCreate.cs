a1c4c15963eaf2b8a65636815dfb82c1da94c05d
Garry Banks <ellisbanks24@gmail.com>
2022-01-17 22:03:42 -0500

    Navbar add to the Note/Index

---
+++ ElevenNote.Models/NoteModels/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models.NoteModels
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least two characters.")]
+        [MaxLength(100, ErrorMessage = "Too many characters within this response.")]
+        public string Title { get; set; }
+
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
