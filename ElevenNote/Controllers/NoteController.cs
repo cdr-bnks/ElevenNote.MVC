9c00fb052ea3505c7443573854fc0373e444420e
Garry Banks <ellisbanks24@gmail.com>
2022-01-18 22:06:58 -0500

    Note/Edit cshtml has been changed

--- ElevenNote/Controllers/NoteController.cs
+++ ElevenNote/Controllers/NoteController.cs
@@ -67,5 +67,45 @@ namespace ElevenNote.Controllers
 
             return View(model);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content,
+                };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id does not match");
+                return View(model);
+            }
+
+            var service = CreateNoteService();
+
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was successfully updated.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your note could not be changed.");
+            return View();
+        }
     }
 }
